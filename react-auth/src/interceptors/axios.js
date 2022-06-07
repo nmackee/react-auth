085540a9edfa00ef13bf52ca58354cc2734985dc
nmackee <mackee.base@gmail.com>
2022-06-07 18:22:03 +0900

    modify eslint error

--- react-auth/src/interceptors/axios.js
+++ react-auth/src/interceptors/axios.js
@@ -2,24 +2,26 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8000/api/';
 
-let refresh = false
+let refresh = false;
 
 axios.interceptors.response.use(
   (resp) => resp,
   (error) => {
     if (error.response.status === 401 && refresh) {
-      refresh = true
+      refresh = true;
 
-      const response = await axios.post('refresh', {}, {withCredentials: true})
+      const response = axios.post('refresh', {}, { withCredentials: true });
 
       if (response.status === 200) {
-        axios.defaults.headers.common['Authorization'] = `Bearer ${response.data['token']}`
+        axios.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${response.data['token']}`;
 
-        return axios(error.config)
+        return axios(error.config);
       }
     }
 
-    refresh = false
-    return error
+    refresh = false;
+    return error;
   }
 );
--- react-auth/src/pages/Home.tsx
+++ react-auth/src/pages/Home.tsx
@@ -4,40 +4,45 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
-
 const Home = (props: { name: string }) => {
   const [name, setName] = useState('');
-  const [navigate, setNavigate] = useState(false)
+  const [navigate, setNavigate] = useState(false);
 
+  /* eslint-disable*/
   useEffect(() => {
-    async () => {
+    async (): Promise<void> => {
       try {
         const { data } = await axios.get('user');
         setName(data.name);
-      } catch(e) {
-        setNavigate(true)
+      } catch (e) {
+        setNavigate(true);
       }
-      
-
-      
     };
   }, []);
 
   const logout = async () => {
-    await axios.post('logout', {}, {withCredentials: true})
+    await axios.post('logout', {}, { withCredentials: true });
 
-    setNavigate(true)
-  }
+    setNavigate(true);
+  };
 
   if (navigate) {
     return <Navigate to="/login" />;
   }
 
-  return <div>
-    <h3>{props.name ? 'Hi ' + props.name : 'You are not logged in'}></h3>
+  return (
+    <div>
+      <h3>{props.name ? 'Hi ' + props.name : 'You are not logged in'}</h3>
 
-    <a href="javascript:void(0)" className='btn bun-lg btn-primary' onClick={logout}>Logout</a>
-    </div>;
+      <a
+        href="javascript:void(0)"
+        className="btn bun-lg btn-primary"
+        onClick={logout}
+      >
+        Logout
+      </a>
+    </div>
+  );
 };
 
 export default Home;
